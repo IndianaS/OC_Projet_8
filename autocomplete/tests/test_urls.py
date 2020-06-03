c3b55a0a2981ba7cc519d2e59dc0cb615d037e94
Rejaud Benjamin <benjamin.rejaud@gmail.com>
2020-06-03 14:53:38 +0200

    Add Test for new fonctionality

---
+++ autocomplete/tests/test_urls.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+from django.urls import resolve
+from django.shortcuts import reverse
+
+from autocomplete.views import complete
+
+
+class UrlTestCase(TestCase):
+
+    def test_complete_url_view(self):
+        found = resolve(reverse("complate"))
+        self.assertEqual(found.func, complete)
---
+++ autocomplete/tests/test_views.py
@@ -0,0 +1,7 @@
+from django.test import TestCase
+
+
+class AutocompleteViews(TestCase):
+
+    def test_views_autocomplete_complete(self):
+        pass
