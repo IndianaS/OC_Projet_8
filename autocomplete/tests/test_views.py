329aaa82b08acc8c5d2bbb4a264017f16ca32bb4
Rejaud Benjamin <benjamin.rejaud@gmail.com>
2020-06-04 16:29:10 +0200

    Add Test autocomplete

--- autocomplete/tests/test_views.py
+++ autocomplete/tests/test_views.py
@@ -1,7 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
+import json
 
 
 class AutocompleteViews(TestCase):
 
     def test_views_autocomplete_complete(self):
-        pass
+        response = self.client.get(reverse("autocomplete/complete", {
+            "term" : "pizz"
+        }))
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(response.content, json.dumps("pizza"))
